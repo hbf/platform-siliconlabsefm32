9c72f62bb31567465af34a8f96b389ae19ece6a4
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2025-01-04 20:08:47 +0100

    Add OpenOCD upload and debug

--- platform.py
+++ platform.py
@@ -17,17 +17,30 @@ import os
 import sys
 
 from platformio.public import PlatformBase
+from platformio import util
 
 IS_WINDOWS = sys.platform.startswith("win")
 
 
 class Siliconlabsefm32Platform(PlatformBase):
 
+    silabs_openocd = {
+        # Windows
+        "windows_amd64": "https://github.com/maxgerhardt/tool-openocd-silabs.git#windows_x64",
+        # No Windows ARM64 or ARM32 builds.
+        # Linux
+        "linux_x86_64": "https://github.com/maxgerhardt/tool-openocd-silabs.git#linux_x64",
+        # no other linux builds
+        # Mac (Intel and ARM are separate)
+        "darwin_x86_64": "https://github.com/maxgerhardt/tool-openocd-silabs.git#linux_x64#mac",
+        "darwin_arm64": "https://github.com/maxgerhardt/tool-openocd-silabs.git#linux_x64#mac"
+    }
     def is_embedded(self):
         return True
 
     def configure_default_packages(self, variables, targets):
         board = variables.get("board")
+        board_config = self.board_config(board)
         frameworks = variables.get("pioframework", [])
         if "mbed" in frameworks:
             self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.90201.0"
@@ -38,6 +51,24 @@ class Siliconlabsefm32Platform(PlatformBase):
                     self.packages[p]["optional"] = False
             if not IS_WINDOWS:
                 self.packages["tool-gperf"]["optional"] = False
+
+        sys_type = util.get_systype()
+        if "tool-openocd-silabs" in self.packages and sys_type in Siliconlabsefm32Platform.silabs_openocd:
+            self.packages["tool-openocd-silabs"]["version"] = Siliconlabsefm32Platform.silabs_openocd[sys_type]
+
+        # configure J-LINK tool
+        jlink_conds = [
+            "jlink" in variables.get(option, "")
+            for option in ("upload_protocol", "debug_tool")
+        ]
+        if board:
+            jlink_conds.extend([
+                "jlink" in board_config.get(key, "")
+                for key in ("debug.default_tools", "upload.protocol")
+            ])
+        jlink_pkgname = "tool-jlink"
+        if not any(jlink_conds) and jlink_pkgname in self.packages:
+            del self.packages[jlink_pkgname]
 
         return super().configure_default_packages(variables, targets)
 
@@ -60,13 +91,28 @@ class Siliconlabsefm32Platform(PlatformBase):
             debug["tools"] = {}
 
         # J-Link / BlackMagic Probe
-        for link in ("blackmagic", "jlink"):
+        for link in ("blackmagic", "jlink", "cmsis-dap"):
             if link not in upload_protocols or link in debug["tools"]:
                 continue
             if link == "blackmagic":
                 debug["tools"]["blackmagic"] = {
                     "hwids": [["0x1d50", "0x6018"]],
                     "require_debug_port": True
+                }
+            elif link == "cmsis-dap": 
+                openocd_target = debug.get("openocd_target")
+                assert openocd_target, ("Missing target configuration for %s" %
+                                        board.id)
+                debug["tools"][link] = {
+                    "server": {
+                        "executable": "bin/openocd",
+                        "package": "tool-openocd-silabs",
+                        "arguments": [
+                            "-s", "$PACKAGE_DIR/share/openocd/scripts",
+                            "-f", "interface/%s.cfg" % link,
+                            "-f", "target/%s" % openocd_target
+                        ]
+                    }
                 }
             else:
                 assert debug.get("jlink_device"), (
@@ -93,7 +139,12 @@ class Siliconlabsefm32Platform(PlatformBase):
 
     def configure_debug_session(self, debug_config):
         if debug_config.speed:
-            if "jlink" in (debug_config.server or {}).get("executable", "").lower():
+            server_executable = (debug_config.server or {}).get("executable", "").lower()
+            if "openocd" in server_executable:
+                debug_config.server["arguments"].extend(
+                    ["-c", "adapter speed %s" % debug_config.speed]
+                )
+            elif "jlink" in server_executable:
                 debug_config.server["arguments"].extend(
                     ["-speed", debug_config.speed]
                 )
